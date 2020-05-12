3b326333f427aac4cca7ed9451e9ded867cfbb5b
Hugo <hblamont@mac.com>
2020-05-12 02:16:27 +0200

    Book detail modal

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react'
+import Modal from './Modal'
 
 
 class Book extends Component {
+    state = {
+      show: false
+    };
+    showModal = e => {
+      this.setState({
+        show: !this.state.show
+      });
+    };
+    onClose = e => {
+      this.props.onClose && this.props.onClose(e);
+    };
     render () {
         const { book, shelf } = this.props
         return (
             <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+                <button class="book-details" onClick={(event) => this.showModal()}> </button>
+                <Modal onClose={this.showModal} show={this.state.show}>
+                  <div>{`Average Note : ${book.averageRating}`}</div>
+                  <div>{`${book.pageCount} pages`}</div>
+                </Modal>
+              </div>
               <div className="book-shelf-changer">
                 <select onChange = {(event) => this.props.onMoveBook(book, event.target.value)}>
                   <option value="move" disabled>Move to...</option>
